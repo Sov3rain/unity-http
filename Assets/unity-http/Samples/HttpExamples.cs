496775a33d293dac314a68ec3d3dff8f7b011b84
Sov3rain <10755450+Sov3rain@users.noreply.github.com>
2025-04-09 16:59:27 +0200

    update examples

--- Assets/unity-http/Samples/HttpExamples.cs
+++ Assets/unity-http/Samples/HttpExamples.cs
@@ -1,31 +1,41 @@
+using System;
+using System.Threading.Tasks;
 using UnityHttp;
 using UnityEngine;
 
 public class HttpExamples : MonoBehaviour
 {
     [ContextMenu(nameof(SimpleGet))]
-    private async void SimpleGet()
+    private async Task SimpleGet()
+    {
+        var response = await Http.Get("https://jsonplaceholder.typicode.com/todos/1").SendAsync();
+        var todo = JsonUtility.FromJson<Todo>(response.Text);
+        Debug.Log(todo);
+    }
+
+    [ContextMenu(nameof(SimpleGetThrows))]
+    private async Task SimpleGetThrows()
     {
         try
         {
-            var response = await Http.Get("https://jsonplaceholder.typicode.com/todosa/1").SendAsync();
+            var response = await Http.Get("https://jsonplaceholder.typicode.com/todoss/1").SendAsync();
             Debug.Log(response.Text);
         }
         catch (HttpException e)
         {
-            Debug.LogError(e.Response.StatusCode);
+            Debug.LogError(e.Message);
         }
     }
 
     [ContextMenu(nameof(ReuseRequest))]
-    private async void ReuseRequest()
+    private async Task ReuseRequest()
     {
         var request = Http.Get("https://jsonplaceholder.typicode.com/todos/1");
         try
         {
             var response = await request.SendAsync();
             Debug.Log(response.Text);
-            
+
             var response2 = await request.SendAsync();
             Debug.Log(response2.Text);
         }
@@ -34,4 +44,15 @@ public class HttpExamples : MonoBehaviour
             Debug.LogError(e.Response.StatusCode);
         }
     }
+}
+
+[Serializable]
+public class Todo
+{
+    public int userId;
+    public int id;
+    public string title;
+    public bool completed;
+    
+    public override string ToString() => $"{title}: {completed}";
 }
